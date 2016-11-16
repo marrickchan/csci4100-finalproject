801daddecaa447beda47e2780efd96db00d4d67e
marrickchan <marrickchan@gmail.com>
2016-11-16 13:49:05 -0500

    Added first stage layout and tested clicking

---
+++ app/src/main/java/net/uoit/mcjb/csci4100_finalproject/World1Stage1.java
@@ -0,0 +1,13 @@
+package net.uoit.mcjb.csci4100_finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class World1Stage1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_world1_stage1);
+    }
+}
